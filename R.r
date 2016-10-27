68fae3d0042aa708c1690d9f9c28ba901c62604b
Reed Jessen <reed@algo.ventures>
2016-10-27 13:06:02 -0700

    Added keyphrase example

--- R.r
+++ R.r
@@ -2,14 +2,25 @@ library(httr)
 
 
 get_ngrams_from_grant_number <- function(grant_number, api_key){
-  
+
   r = POST(url = "https://api.ipstreet.com/v2/claim_parser/ngram",
            add_headers(`x-api-key`=api_key),
            body = list(q=list(grant_number=grant_number)),
            encode ="json")
-  
+
   return(content(r))
-  
+
+}
+
+get_keyphrases_from_grant_number <- function(grant_number, api_key){
+
+  r = POST(url = "https://api.ipstreet.com/v2/claim_parser/keyphrase",
+           add_headers(`x-api-key`=api_key),
+           body = list(q=list(grant_number=grant_number)),
+           encode ="json")
+
+  return(content(r))
+
 }
 
 download_csv_from_response <- function(response, download_target_location){
@@ -19,8 +30,15 @@ download_csv_from_response <- function(response, download_target_location){
 
 #configuartion parameters
 api_key = "YOUR_API_KEY_GOES_HERE"
-download_target_location = "YOUR_DOWNLOAD_TARGET_LOCATION_GOES_HERE"
+ngram_download_target_location = "C:/Users/reedj_000/Desktop/Schwegment/output/ngram-"
+keyphrase_download_target_location = "C:/Users/reedj_000/Desktop/Schwegment/output/keyphrase-"
 
 
-response = get_ngrams_from_grant_number('7546750', api_key)
-download_csv_from_response(response,download_target_location)
+#send a ngram request
+ngram_response = get_ngrams_from_grant_number('7546750', api_key)
+#send a keyphrase request
+keyphrase_response = get_keyphrase_from_grant_number('7546750', api_key)
+
+#save both requests to csv files
+download_csv_from_response(ngram_response,ngram_download_target_location)
+download_csv_from_response(keyphrase_response,keyphrase_download_target_location)
