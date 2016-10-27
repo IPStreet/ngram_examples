7ab83671c2ba77ef08fcb9a5c181810248f1ecf3
Reed Jessen <reed@algo.ventures>
2016-10-27 13:01:27 -0700

    Added keyphrase example

--- python.py
+++ python.py
@@ -20,10 +20,27 @@ def get_ngrams_from_grant_number(grant_number, api_key):
 
     return response
 
+def get_keyphrases_from_grant_number(grant_number, api_key):
+    """
+
+    :param grant_number: a patent grant number
+    :param api_key: client api key
+    :return: keyphrases json object
+    """
+    endpoint = 'https://api.ipstreet.com/v2/claim_parser/keyphrase'
+    headers = {'x-api-key': api_key}
+    payload = json.dumps({'q': {'grant_number': grant_number}})
+    r = requests.post(endpoint, headers=headers, data=payload)
+
+    response = r.json()
+
+    return response
+
+
 def download_csv_from_response(response,download_target_location):
     """
 
-    :param response: a json response from the /claim_parser/ngram endpoint
+    :param response: a json response from the /claim_parser/ endpoint
     :param download_target_location: where you want the csv file to be downloaded to
     :return: void
     """
@@ -42,11 +59,15 @@ def download_csv_from_response(response,download_target_location):
 
 if __name__ == "__main__":
 
-    api_key = "YOUR_API_KEY_GOES_HERE"
+    api_key = "YOUR-API-KEY"
 
-    response = get_ngrams_from_grant_number('7546750', api_key)
-    print(response)
+    ngram_response = get_ngrams_from_grant_number('7546750', api_key)
+    print(ngram_response)
+    keyphrase_response = get_keyphrases_from_grant_number('7546750', api_key)
+    print(keyphrase_response)
 
-    download_target_location = 'YOUR_DOWNLOAD_TARGET_LOCATION_GOES_HERE'
+    ngram_download_target_location = 'C:/Users/reedj_000/Desktop/Schwegment/output/ngram-'
+    keyphrase_download_target_location = 'C:/Users/reedj_000/Desktop/Schwegment/output/keyphrase-'
 
-    download_csv_from_response(response,download_target_location)
+    download_csv_from_response(ngram_response,ngram_download_target_location)
+    download_csv_from_response(keyphrase_response, keyphrase_download_target_location)
